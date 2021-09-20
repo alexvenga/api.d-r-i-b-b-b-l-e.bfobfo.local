ff7ab9c0e06253338a0a246168b933c9bff215b9
AlexVenga <bubnov.alexey@gmail.com>
2021-09-21 00:11:26 +0300

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,28 @@ module.exports = {
     ],
 
     theme: {
+        screens: {
+            'xs': '360px',
+            ...defaultTheme.screens,
+        },
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '0.5rem',
+                // xs: '0.5rem',
+                sm: '1rem',
+                // lg: '4rem',
+                // xl: '5rem',
+                // '2xl': '6rem',
+            },
+            screens: {
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
+                '2xl': '1536px',
+            },
+        },
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
